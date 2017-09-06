19e99ef84f50b1351f50704793fe83274be06d7e
RBK12 <rbk12@RBK12s-Mac-mini.local>
2017-09-06 16:02:54 +0300

    grosary items modified

--- app.js
+++ app.js
@@ -12,3 +12,20 @@ var Potato = () => (
 	);
 
 ReactDOM.render(<App />,document.getElementById("app"));
+
+var GroceryListItem = () => (
+  <div>
+    <h2>My Grocery List</h2>
+    <GroceryList items={['Tomato', 'Potato']} /> 
+  </div>
+);
+
+var GroceryList = (props) => (
+  <ul>
+    <li>{props.items[0]}</li>
+    <li>{props.items[1]}</li>
+  </ul>
+);
+
+
+ReactDOM.render(<GroceryListItem />, document.getElementById("epp"));
