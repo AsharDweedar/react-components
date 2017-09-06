a031db70255bece7bf85e55339427c967e933a2a
RBK12 <rbk12@RBK12s-Mac-mini.local>
2017-09-06 19:07:45 +0300

    React component final editing

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var App = ()=>(
+/*var App = ()=>(
 	<ul>
 		<Tomato />
 		<Potato />
@@ -31,6 +31,42 @@ var GroceryList = (props) => {
 		</ul>
 	);
 }
-
-
+/Users/rbk12/Library/Application Support/Sublime Text 3/Installed Packages
 ReactDOM.render(<GroceryListItem />, document.getElementById("epp"));
+*/
+class GroceryListItem extends React.Component {
+	constructor(props) {
+	    super(props);
+	    this.state = {
+	    	done:false
+	    };
+	}
+	onListItemClick() {
+	    this.setState({
+	      done: !this.state.done
+	    });
+	}
+
+	render() {
+		var style = { 
+	      fontWeight: this.state.done ? 'bold' : 'normal'
+	      //textDecoration : this.state.done ? 'line-through' : 'none'
+	    };
+	    return (
+	      <li style={style} onClick={this.onListItemClick.bind(this)}> type : {this.props.item} </li>
+	    );
+	}
+}
+
+
+
+var GroceryList = (props) => (
+		<ul>
+		  {props.items.map( i => <GroceryListItem item={i}/>)}
+		</ul>
+)
+
+
+ReactDOM.render(<GroceryList items={['Tomato', 'Potato']}/>, document.getElementById("epp"));
+
+
